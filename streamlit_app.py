3818eeae30bda8d8d4700e001b0b7d6b939fbc69
Ojelle M. Rogero <ojelle.rogero@gmail.com>
2022-08-28 14:43:50 +0800

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Read the fruit_macros.txt from your AWS S3 bucket
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-fruit_index = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Pick list to interact what fruit they want to include
-fruits_selected = streamlit.multiselect('Pick some fruits: ', list(fruit_index.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page
