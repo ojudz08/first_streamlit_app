a7b7cd47a212d090b5360692ea78e4eec46e9a9b
Ojelle M. Rogero <ojelle.rogero@gmail.com>
2022-08-28 14:06:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(':oatmeal: Omega 3 & Blueberry Oatmeal')
+streamlit.text(':cereal: Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
