383461740cd948154e3d9a404249ad35ba8cac04
Ojelle M. Rogero <ojelle.rogero@gmail.com>
2022-08-31 22:58:14 +0800

    Update streamlit_app.py
    organized functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,38 +2,41 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
-from urllib.error import URLError
 
-streamlit.title("My Mom's New Healthy Diner")
-
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
-# Read the fruit_macros.txt from your AWS S3 bucket
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Pick list to interact what fruit they want to include
-fruits_selected = streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page
-streamlit.dataframe(fruits_to_show)
-
-# Create function to get fruit_choice
+# Get fruit_choice function
 def get_fruityvice_data(fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  # requests
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())                   # Take the json version of the response and normalize it
   return fruityvice_normalized
 
-# Display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+    return my_cur.fetchall()
+
+
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('" + new_fruit + "')")
+    return f'Thanks for adding {new_fruit}'
+
+  
+streamlit.title("My Mom's New Healthy Diner")
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')       # Read the fruit_macros.txt from your AWS S3 bucket
+my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected = streamlit.multiselect('Pick some fruits: ', list(my_fruit_list.index), ['Avocado', 'Strawberries'])     # Pick list to interact what fruit they want to include
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)                   # Display the table on the page
+
+streamlit.header('Fruityvice Fruit Advice!')          # Display fruityvice api response
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
   if not fruit_choice:
@@ -44,25 +47,12 @@ try:
 except Exception as e:
   print(f'{e}')
 
-
 streamlit.header("View Our Fruit List - Add Your Favorites")
-def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
-    return my_cur.fetchall()
- 
-# Add button to load the fruit
-if streamlit.button('Get Fruit List'):
+if streamlit.button('Get Fruit List'):            # Add button to load the fruit
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
-
-# Allow the end user to add a fruit to the list
-def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('" + new_fruit + "')")
-    return f'Thanks for adding {new_fruit}'
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 if streamlit.button('Add a Fruit to the List'):
